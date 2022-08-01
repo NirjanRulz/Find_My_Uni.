4f3e64e0095723aee99b037ae3d016ded3f2ab89
NirjanRulz <65400597+NirjanRulz@users.noreply.github.com>
2022-08-01 22:37:46 +0600

    Add files via upload

---
+++ university.sql
@@ -0,0 +1,210 @@
+create table NSU_Program_and_Credits ( 
+Varsity_name varchar (15), 
+Program_School varchar (50), 
+Program_Names varchar (50),
+Total_Credits numeric (5) 
+); 
+
+insert into NSU_Program_and_Credits values 
+('North South University', 'School of Engineering & Physical Sciences', 'Bachelor of Architecture', 170),
+('North South University', 'School of Engineering & Physical Sciences', 'BS in Civil & Environmental Engineering (CEE)', 149),
+('North South University', 'School of Engineering & Physical Sciences', 'BS in Computer Science & Engineering (CSE)', 130),
+('North South University', 'School of Engineering & Physical Sciences', 'BS in Electrical & Electronic Engineering (EEE)', 130),
+('North South University', 'School of Engineering & Physical Sciences', 'BS in Electronic & Telecom Engineering (ETE)', 130),
+('North South University', 'School of Health and Life Sciences', 'BS in Biochemistry and Biotechnology', 120),
+('North South University', 'School of Health and Life Sciences', 'BS in Environmental Science & Management', 130),
+('North South University', 'School of Health and Life Sciences', 'BS in Microbiology', 120),
+('North South University', 'School of Health and Life Sciences', 'BPharm Professional', 199),
+('North South University', 'School of Business & Economics', 'BBA Major in Accounting', 120),
+('North South University', 'School of Business & Economics', 'BBA Major in Economics', 124),
+('North South University', 'School of Business & Economics', 'BBA Major in Entrepreneurship', 124),
+('North South University', 'School of Business & Economics', 'BBA Major in Finance', 120),
+('North South University', 'School of Business & Economics', 'BBA Major in Human Resource Management', 124),
+('North South University', 'School of Business & Economics', 'BBA Major in International Business', 124),
+('North South University', 'School of Business & Economics', 'BBA Major in Management', 124),
+('North South University', 'School of Business & Economics', 'BBA Major in Management Information Systems', 124),
+('North South University', 'School of Business & Economics', 'BBA Major in Marketing', 130),
+('North South University', 'School of Business & Economics', 'BBA Major in Supply Chain Management', 124),
+('North South University', 'School of Business & Economics', 'BBA General', 124),
+('North South University', 'School of Business & Economics', 'BS in Economics', 125),
+('North South University', 'School of Humanities & Social Sciences', 'BA in English', 123),
+('North South University', 'School of Humanities & Social Sciences', 'Bachelor of Laws (LLB Hons)', 130);
+
+create table IU_Program_and_Credits ( 
+Varsity_name varchar (15), 
+Program_School varchar (50), 
+Program_Names varchar (50),
+Total_Credits numeric (5) 
+); 
+
+insert into IU_Program_and_Credits values 
+('Independent University', 'School of Business and Entrepreneurship', 'Bachelor of Business Administration (BBA) in Accounting', 125),
+('Independent University', 'School of Business and Entrepreneurship', 'Bachelor of Business Administration (BBA) in Finance', 125),
+('Independent University', 'School of Business and Entrepreneurship', 'Bachelor of Business Administration (BBA) in General Management', 125),
+('Independent University', 'School of Business and Entrepreneurship', 'Bachelor of Business Administration (BBA) in Accounting', 124),
+('Independent University', 'School of Business and Entrepreneurship', 'Bachelor of Business Administration (BBA) in Human Resource Management (HRM)', 124),
+('Independent University', 'School of Business and Entrepreneurship', 'Bachelor of Business Administration (BBA) in Marketing', 125),
+('Independent University', 'School of Business and Entrepreneurship', 'Bachelor of Business Administration (BBA) in Management Information System (MIS)', 125),
+('Independent University', 'School of Business and Entrepreneurship', 'Bachelor of Business Administration (BBA) in International Business', 125),
+('Independent University', 'School of Business and Entrepreneurship', 'Bachelor of Science (B.Sc.) in Economics', 122),
+('Independent University', 'School of Engineering, Technology and Science (B.Sc.)', 'BS in Computer Engineering (CE)', 134),
+('Independent University', 'School of Engineering, Technology and Science (B.Sc.)', 'BS in Computer Science (CS)', 134),
+('Independent University', 'School of Engineering, Technology and Science (B.Sc.)', 'BS in Computer Science & Engineering (CSE)', 143),
+('Independent University', 'School of Engineering, Technology and Science (B.Sc.)', 'BS in Electrical & Electronic Engineering (EEE)', 147),
+('Independent University', 'School of Engineering, Technology and Science (B.Sc.)', 'BS in Electronic & Telecommunication Engineering (ETE)', 145),
+('Independent University', 'School of Engineering, Technology and Science (B.Sc.)', 'BS in Mathematics',  140),
+('Independent University', 'School of Engineering, Technology and Science (B.Sc.)', 'BS in Physics',  140),
+('Independent University', 'School of Environment And Life Sciences', 'Bachelor in Environmental Science and Management',  150),
+('Independent University', 'School of Environment And Life Sciences', 'Bachelor in Microbiology',  159),
+('Independent University', 'School of Environment And Life Sciences', 'Bachelor in Biochemistry and Biotechnology',  170),
+('Independent University', 'School of Liberal Arts & Social Sciences', 'Bachelor of Social Science (BSS) in Media & Communication',  127),
+('Independent University', 'School of Liberal Arts & Social Sciences', 'Bachelor of Social Science (BSS) in Anthropology',  127),
+('Independent University', 'School of Liberal Arts & Social Sciences', 'Bachelor of Social Science (BSS) in Sociology',  126),
+('Independent University', 'School of Liberal Arts & Social Sciences', 'Bachelor of Social Science (BSS) in Global Studies and Governance (GSG)',  128),
+('Independent University', 'School of Liberal Arts & Social Sciences', 'Bachelor of Social Science (BSS) in LLB(Hons)', 130),
+('Independent University', 'School of Liberal Arts & Social Sciences', 'Bachelor of Arts (BA) in English Literature',  125),
+('Independent University', 'School of Liberal Arts & Social Sciences', 'Bachelor of Arts (BA) in English Language Teaching (ELT)',  124),
+('Independent University', 'School of Pharmacy and Public Health', 'Bachelor of Pharmacy (BPharm)', 190); 
+
+create table BracU_Program_and_Credits ( 
+Varsity_name varchar (15), 
+Program_School varchar (50), 
+Program_Names varchar (50),
+Total_Credits numeric (5) 
+); 
+
+insert into BracU_Program_and_Credits values 
+('Brac University', 'School of Humanities & Social Sciences', 'Bachelor of Social Sciences in Anthropology', 120),
+('Brac University', 'School of Humanities & Social Sciences', 'Bachelor of Social Science in Economics', 123),
+('Brac University', 'School of Humanities & Social Sciences', 'Bachelor of Laws (LLB Hons)', 130),
+('Brac University', 'School of Liberal Arts & Social Sciences', 'Bachelor of Arts (BA) in English', 120),
+('Brac University', 'School of Business & Economics', 'Bachelor of Business Administration (BBA)', 120),
+('Brac University', 'School of Engineering & Physical Sciences', 'BS in Applied Physics & Electronics', 130),
+('Brac University', 'School of Engineering & Physical Sciences', 'Bachelor of Architecture', 199),
+('Brac University', 'School of Engineering & Physical Sciences', 'BS in Computer Science (CE)', 136),
+('Brac University', 'School of Engineering & Physical Sciences', 'BS in Computer Science & Engineering (CSE)', 136),
+('Brac University', 'School of Engineering & Physical Sciences', 'BS in Electrical & Electronic Engineering (EEE)', 136),
+('Brac University', 'School of Engineering & Physical Sciences', 'BS in Electronic & Communication Engineering (ECE)', 136),
+('Brac University', 'School of Engineering & Physical Sciences', 'BS in Mathematics',  127),
+('Brac University', 'School of Engineering & Physical Sciences', 'BS in Physics',  132),
+('Brac University', 'School of Health and Life Sciences', 'BS in Biotechnology', 136),
+('Brac University', 'School of Health and Life Sciences', 'BS in Microbiology', 136),
+('Brac University', 'School of Health and Life Sciences', 'Bachelor of Pharmacy (Hons)', 164);
+
+create table DU_Program_and_Credits ( 
+Varsity_name varchar (15), 
+Program_School varchar (50), 
+Program_Names varchar (50),
+Total_Credits numeric (5) 
+); 
+
+insert into DU_Program_and_Credits values 
+('Daffodil University', 'School of Business and Entrepreneurship', 'Bachelor of Business Administration (BBA)', 125),
+('Daffodil University', 'School of Business and Entrepreneurship', 'Bachelor of Business Studies (BBS)', 134),
+('Daffodil University', 'School of Business and Entrepreneurship', 'Bachelor of Real Estate (BRE)', 148),
+('Daffodil University', 'School of Business and Entrepreneurship', 'Bachelor of Tourism & Hospitality Management (BTHM)', 134),
+('Daffodil University', 'School of Business and Entrepreneurship', 'Bachelor of Innovation & Entrepreneurship (BE)', 135),
+('Daffodil University', 'School of Science and Information Technology', 'BS in Computer Science & Engineering (CSE)', 142),
+('Daffodil University', 'School of Science and Information Technology', 'BS in Software Engineering (SWE)', 145),
+('Daffodil University', 'School of Science and Information Technology', 'BS in Environmental Science and Disaster Management (ESDM)', 140),
+('Daffodil University', 'School of Science and Information Technology', 'BS in Multimedia & Creative Technology (MCT)', 147),
+('Daffodil University', 'School of Science and Information Technology', 'BS in General Educational Development (GED)', 130),
+('Daffodil University', 'School of Science and Information Technology', 'BS in Information Technology & Management (ITM)', 136),
+('Daffodil University', 'School of Engineering', 'B.Sc. in Electronics & Telecommunication Engineering (ETE)', 145),
+('Daffodil University', 'School of Engineering', 'B.Sc. in Textile Engineering (TE)', 144),
+('Daffodil University', 'School of Engineering', 'B.Sc. in Electrical & Electronic Engineering (EEE)', 140),
+('Daffodil University', 'School of Engineering', 'B.Sc. in Architecture (B. Arch)', 180),
+('Daffodil University', 'School of Engineering', 'Department of Civil Engineering (CE)', 149),
+('Daffodil University', 'School of Allied Health Science', 'Bachelor of Pharmacy (Hons)', 162),
+('Daffodil University', 'School of Allied Health Science', 'B.Sc. in Nutrition & Food Engineering (NFE)', 144),
+('Daffodil University', 'School of Allied Health Science', 'Bachelor of Arts (Hons) in English', 126),
+('Daffodil University', 'School of Allied Health Science', 'Bachelor of Law (Hons)(LLB)', 144),
+('Daffodil University', 'School of Allied Health Science', 'BSS (Hons) in Journalism, Media and Communication', 142);
+
+create table AIUB_Program_and_Credits ( 
+Varsity_name varchar (15), 
+Program_School varchar (50), 
+Program_Names varchar (50),
+Total_Credits numeric (5) 
+); 
+
+insert into AIUB_Program_and_Credits values 
+('American International University-Bangladesh', 'School of Arts and Social Sciences', 'Bachelor of Arts (BA) in Media and Mass Communication', 125),
+('American International University-Bangladesh', 'School of Arts and Social Sciences', 'Bachelor of Arts (BA) in English', 124),
+('American International University-Bangladesh', 'School of Arts and Social Sciences', 'Bachelor of Law (LLB)', 130),
+('American International University-Bangladesh', 'School of Arts and Social Sciences', 'Bachelor of Social Science (BSS) in Economics', 126),
+('American International University-Bangladesh', 'School of Business Administration', 'Bachelor of Business Administration in Accounting', 120),
+('American International University-Bangladesh', 'School of Business Administration', 'Bachelor of Business Administration in Economics', 123),
+('American International University-Bangladesh', 'School of Business Administration', 'Bachelor of Business Administration in Finance', 123),
+('American International University-Bangladesh', 'School of Business Administration', 'Bachelor of Business Administration in Human Resource Management (HRM)', 125),
+('American International University-Bangladesh', 'School of Business Administration', 'Bachelor of Business Administration in International Business (IB)', 126),
+('American International University-Bangladesh', 'School of Business Administration', 'Bachelor of Business Administration in Investment Management (IM)', 124),
+('American International University-Bangladesh', 'School of Business Administration', 'Bachelor of Business Administration in Management', 124),
+('American International University-Bangladesh', 'School of Business Administration', 'Bachelor of Business Administration in Management Information Systems (MIS)', 127),
+('American International University-Bangladesh', 'School of Business Administration', 'Bachelor of Business Administration in Marketing', 170),
+('American International University-Bangladesh', 'School of Business Administration', 'Bachelor of Business Administration in Operations and Supply Chain Management (OSCM)', 130),
+('American International University-Bangladesh', 'School of Business Administration', 'Bachelor of Business Administration in Tourism and Hospitality Management (THM)', 130),
+('American International University-Bangladesh', 'School of Engineering', 'Bachelor of Architecture (BArch)', 170),
+('American International University-Bangladesh', 'School of Engineering', 'Bachelor of Science (BSc) in Electrical and Electronic Engineering (EEE)', 145),
+('American International University-Bangladesh', 'School of Engineering', 'Bachelor of Science (BSc) in Computer Engineering  (CE)', 150),
+('American International University-Bangladesh', 'School of Engineering', 'Bachelor of Science (BSc) in Industrial and Production Engineering (IPE)', 165),
+('American International University-Bangladesh', 'School of Science and Technology', 'Bachelor of Science (BSc) in Computer Science and Engineering (CSE)', 145);
+
+create table SEU_Program_and_Credits ( 
+Varsity_name varchar (15), 
+Program_School varchar (50), 
+Program_Names varchar (50),
+Total_Credits numeric (5) 
+); 
+
+insert into SEU_Program_and_Credits values 
+('Southeast University', 'School of Science & Engineering', 'BSc in CSE', 150),
+('Southeast University', 'School of Science & Engineering', 'BSc in EEE', 153),
+('Southeast University', 'School of Science & Engineering', 'BSc in Textile Engg.', 154),
+('Southeast University', 'School of Science & Engineering', 'B. Pharm', 170),
+('Southeast University', 'School of Science & Engineering', 'B. Architecture', 190),
+('Southeast University', 'School of Business Administration', 'BBA in General', 127),
+('Southeast University', 'School of Arts and Social Sciences', 'Bachelor of Law (LLB)', 153),
+('Southeast University', 'School of Arts and Social Sciences', 'BA in English', 129),
+('Southeast University', 'School of Arts and Social Sciences', 'BSS in Economics ', 123),
+('Southeast University', 'School of Arts and Social Sciences', 'BA in Bangla', 129);
+
+
+create table Admission_and_Tuition_Fees ( 
+Varsity_Name varchar (15), 
+Admission_Form numeric (10),
+Admission_Fee numeric (10),
+Fees_per_Credit numeric (10), 
+Extra_Fees numeric (10)  
+); 
+
+insert into Admission_and_Tuition_Fees values 
+('North South University', 1000, 35000, 7000, 9000),
+('Independent University', 1000, 25000, 6000, 7000),
+('Brac University', 1000, 28000, 6600, 7000),
+('Daffodil University', 1000, 25000, 4500, 5000),
+('American International University-Bangladesh', 1000, 25000, 5500, 6000),
+('Southeast University', 1000, 15000, 3850, 3500);
+
+
+create table Tuition_Fees_for_full_Program ( 
+Varsity_Name varchar (50), 
+Program_Name varchar (50), 
+Total_Credits numeric (5), 
+Fees_per_Credit numeric (10), 
+Total_Tuition_Fees_for_12_semesters numeric (10), 
+Extra_Fees numeric (10),
+Total_Extra_Fees_for_12_semesters numeric (5)
+); 
+
+insert into Tuition_Fees_for_full_Program values 
+('North South University', 'BS in Computer Science & Engineering (CSE)', 130, 7000, 910000, 9000, 108000),
+('Independent University', 'BS in Computer Science & Engineering (CSE)', 143, 6000, 858000, 7000, 84000),
+('Brac University', 'BS in Computer Science & Engineering (CSE)', 136, 6600, 897600, 7000, 84000),
+('Daffodil University', 'BS in Computer Science & Engineering (CSE)', 142, 4500, 63900, 5000, 60000),
+('American International University-Bangladesh','Bachelor of Science (BSc) in Computer Science and Engineering (CSE)', 145, 5500, 797500, 6000, 72000),
+('Southeast University', 'BSc in CSE', 150, 3850, 57500, 3500, 42000); 
+
+
+
+
